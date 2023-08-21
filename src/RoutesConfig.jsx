f3384a3aeacc7fbdceb027daa56edd2c2e8dde44
Sally Glanowski <sally.glanowski@gmail.com>
2023-08-21 12:39:21 +0200

    configuration des routes

---
+++ src/RoutesConfig.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Logement from "./pages/Logement";
+import PageNotFound from ".pages/PageNotFound"
+
+function ConfigRoutes() {
+  return (
+    <Routes>
+      <Route path="/kasa" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/logement/:logementId" element={<Logement />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
+  );
+}
+
+export default ConfigRoutes;
