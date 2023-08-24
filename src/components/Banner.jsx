cd62cc7b89769478b313ca6d1203743116e7ef68
Sally Glanowski <sally.glanowski@gmail.com>
2023-08-24 18:20:19 +0200

    logo class add + banner style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Banner({ bannerImg, bannerTitle, page }) {
   return (
     <div className={`banner ${page}`}>
-      <img src={bannerImg} alt={bannerTitle} />
+      <img src={bannerImg} alt={bannerTitle} className="banner-image" />
       <h1>{bannerTitle}</h1>
     </div>
   );
