2492d5faf9049ece598da4fd8a5230291e831ac2
Sally Glanowski <sally.glanowski@gmail.com>
2023-08-29 14:52:07 +0200

    return and change version caroussel

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -2,13 +2,16 @@ import React, { useState, useRef } from "react";
 
 function Caroussel({ images }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const carrouselImageRef = useRef(null);
+  const carousselImageRef = useRef(null);
 
   const imageSize = () => {
-    const carrouselImage = carrouselImageRef.current;
+    const carousselImage = carousselImageRef.current;
 
-    if (!carrouselImage) return 0;
-    return carrouselImage.width;
+    if (!carousselImage) {
+      return 0;
+    }
+
+    return carousselImage.width;
   };
 
   function previousImage() {
@@ -23,38 +26,41 @@ function Caroussel({ images }) {
     );
   }
 
-  const singleImage = images.length === 1;
+  if (images.length === 1) {
+    return (
+      <div className="caroussel">
+        <img className="caroussel-image" src={images[0]} alt="Logement" />
+      </div>
+    );
+  }
 
   return (
-    <div className="carrousel">
+    <div className="caroussel">
       <div
-        className="carrousel-container"
-        style={{ transform: `translateX(-${currentImageIndex * imageSize()}px)` }}
-      >
+        className="caroussel-container"
+        style={{
+          transform: `translateX(-${currentImageIndex * imageSize()}px)`,
+        }}>
         {images.map((image, index) => (
           <img
-            className="carrousel-image"
+            className="caroussel-image"
             src={image}
             key={index}
             alt="Logement"
-            ref={carrouselImageRef}
+            ref={carousselImageRef}
           />
         ))}
       </div>
-      <div className="carrousel-navigation">
-        {!singleImage && (
-          <button className="carrousel-button" onClick={previousImage}>
-            &lt;
-          </button>
-        )}
-        <span className="carrousel-counter">
-          {`${currentImageIndex + 1} / ${images.length}`}
-        </span>
-        {!singleImage && (
-          <button className="carrousel-button" onClick={nextImage}>
-            &gt;
-          </button>
-        )}
+      <div className="caroussel-navigation">
+        <button className="caroussel-button" onClick={previousImage}>
+          &lt;
+        </button>
+        <span className="caroussel-counter">{`${currentImageIndex + 1} / ${
+          images.length
+        }`}</span>
+        <button className="caroussel-button" onClick={nextImage}>
+          &gt;
+        </button>
       </div>
     </div>
   );
